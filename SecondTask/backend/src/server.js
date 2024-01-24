f3b30ead4a0aefc9de3f9dee169c69250354bda1
D0mazo <dominykas.simanavicius@proton.me>
2024-01-24 11:08:25 +0200

    Middlewares

--- SecondTask/backend/src/server.js
+++ SecondTask/backend/src/server.js
@@ -1,10 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();  
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+// Middlewares
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+app.listen(port, () => {
     console.log(`Serveris veikia Port ${port}`);
 });
